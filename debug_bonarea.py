286a3e4fe2b369a6f31da900a6c2431f6b41d7a9
David <starstech.solution@gmail.com>
2026-04-03 20:43:41 +0200

    Update debug_bonarea.py

--- debug_bonarea.py
+++ debug_bonarea.py
@@ -16,37 +16,32 @@ def crear_driver():
     service = Service('/usr/bin/chromedriver')
     return webdriver.Chrome(service=service, options=chrome_options)
 
-def comptar_subcategories(base_url, nom):
-    print(f'\n{nom}:')
-    total = 0
-    # Provar _001 primer
-    driver = crear_driver()
-    driver.get(base_url + '_001')
-    time.sleep(6)
-    p001 = len(driver.find_elements(By.CSS_SELECTOR, 'div.block-product'))
-    driver.quit()
-    print(f'  _001 -> {p001} productes')
-
-    # Provar _010, _020...
-    for n in range(1, 15):
-        suffix = f'_{n*10:03d}'
+def decidir_estrategia(base_url, nom):
+    # Comptem _001 i _010
+    for suffix in ['_001', '_010']:
         driver = crear_driver()
         driver.get(base_url + suffix)
         time.sleep(6)
         p = len(driver.find_elements(By.CSS_SELECTOR, 'div.block-product'))
         driver.quit()
-        print(f'  {suffix} -> {p} productes')
-        if p == 0:
-            break
-        total += p
+        if suffix == '_001': p001 = p
+        if suffix == '_010': p010 = p
 
-    print(f'  TOTAL iterant _010... = {total}')
+    # Regla: si _001 >= 3x _010, usar _001
+    if p010 == 0 or p001 >= p010 * 3:
+        estrategia = 'usar _001'
+    else:
+        estrategia = 'iterar _010, _020...'
 
-comptar_subcategories(
-    'https://www.bonarea-online.com/categories/vi-blanc/13_320_090',
-    'Vi blanc'
-)
-comptar_subcategories(
-    'https://www.bonarea-online.com/categories/begudes-refrescants/13_320_030',
-    'Refrescos'
-)
+    print(f'{nom}: _001={p001}, _010={p010} -> {estrategia}')
+
+categories = [
+    ('https://www.bonarea-online.com/categories/vi-blanc/13_320_090', 'Vi blanc'),
+    ('https://www.bonarea-online.com/categories/begudes-refrescants/13_320_030', 'Refrescos'),
+    ('https://www.bonarea-online.com/categories/lactics-i-derivats/13_300_080', 'Lactics'),
+    ('https://www.bonarea-online.com/categories/conserves/13_300_040', 'Conserves'),
+    ('https://www.bonarea-online.com/categories/carns-i-ous/13_300_010', 'Carns'),
+]
+
+for url, nom in categories:
+    decidir_estrategia(url, nom)
