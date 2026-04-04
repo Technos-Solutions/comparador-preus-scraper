dab0f328772eed23702b26ab0f643e7f769d2c7b
David <starstech.solution@gmail.com>
2026-04-04 10:47:03 +0200

    Update debug_bonarea.py

--- debug_bonarea.py
+++ debug_bonarea.py
@@ -10,28 +10,15 @@ def crear_driver():
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--window-size=1920,1080')
-    # Anti-deteccio Cloudflare
-    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-    chrome_options.add_argument('--disable-web-security')
-    chrome_options.add_argument('--allow-running-insecure-content')
-    chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
-    chrome_options.add_experimental_option('useAutomationExtension', False)
-    chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
+    chrome_options.add_argument('user-agent=Mozilla/5.0')
     chrome_options.binary_location = '/usr/bin/chromium-browser'
     from selenium.webdriver.chrome.service import Service
     service = Service('/usr/bin/chromedriver')
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    # Eliminar el flag webdriver
-    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-    return driver
+    return webdriver.Chrome(service=service, options=chrome_options)
 
+# Entrem a Frescos i veiem totes les subcategories
 driver = crear_driver()
-driver.get('https://www.carrefour.es/supermercado/frescos/cat20002/c')
-time.sleep(15)
+driver.get('https://www.compraonline.bonpreuesclat.cat/categories/frescos/c95cfbf2-501d-433f-bae3-10fcef330b11')
+time.sleep(10)
 
-print(f'Titol: {driver.title}')
-noms = driver.find_elements(By.CSS_SELECTOR, 'a.product-card__title-link')
-print(f'Productes: {len(noms)}')
-if noms:
-    print(f'Primer: {noms[0].get_attribute("innerText").strip()}')
-driver.quit()
+links = driver.find_elements(By.CSS_SELECTOR, 'a[href*="/ca
