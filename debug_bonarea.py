c0a6d61f046ebcdab82e6e253bd05294a3589ae9
Technos-Solutions <starstech.solution@gmail.com>
2026-05-02 13:04:15 +0200

    Debug BonPreuEsclat: ruta chromedriver /usr/local/bin

--- debug_bonarea.py
+++ debug_bonarea.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 BASE_URL = 'https://www.compraonline.bonpreuesclat.cat'
@@ -18,7 +17,7 @@ def crear_driver():
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--window-size=1920,1080')
     chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36')
-    service = Service(ChromeDriverManager().install())
+    service = Service('/usr/local/bin/chromedriver')
     return webdriver.Chrome(service=service, options=chrome_options)
 
 print("=" * 60)
