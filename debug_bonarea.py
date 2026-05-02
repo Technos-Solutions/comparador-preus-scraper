8e166d70df540fff23cf60b3c918773ac6589e3c
Technos-Solutions <starstech.solution@gmail.com>
2026-05-02 13:14:59 +0200

    Debug BonPreuEsclat: simplificar opcions Chrome

--- debug_bonarea.py
+++ debug_bonarea.py
@@ -11,16 +11,12 @@ BASE_URL = 'https://www.compraonline.bonpreuesclat.cat'
 
 def crear_driver():
     chrome_options = Options()
-    chrome_options.add_argument('--headless=new')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--disable-setuid-sandbox')
-    chrome_options.add_argument('--single-process')
-    chrome_options.add_argument('--disable-extensions')
-    chrome_options.add_argument('--disable-software-rasterizer')
     chrome_options.add_argument('--window-size=1920,1080')
-    chrome_options.add_argument('--remote-debugging-port=9222')
     chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36')
     service = Service('/usr/bin/chromedriver')
     return webdriver.Chrome(service=service, options=chrome_options)
